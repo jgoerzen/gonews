1166aa87138e03247c35503ec4c7b1efd794137b
jgoerzen <jpgarch@complete.org>
2001-08-29 21:59:09 -0500

    /gonews/head: changeset 23
    Updated
    
    darcs-hash:20010830025909-a5eff-e928e565d7f37ca05f2b588be6787a3f6ec300e6.gz

--- GopherListen.py
+++ GopherListen.py
@@ -9,11 +9,15 @@ class GopherRequest(SocketServer.BaseRequestHandler):
     def handle(self):
         sock = self.request
         sf = sock.makefile("r+")
-        req = sf.readline().strip().split("\t")[0]
-        ntc = NNTPComm.Server((SERVERIP, 1178), NEWSSERVER, sf)
-        ntc.sendnglist()
+        req = sf.readline().strip().split("\t")
+        ntc = NNTPComm.Server((SERVERIP, 1176), NEWSSERVER, sf)
+        if (req[0]):
+            ngs = NNTPComm.Group(ntc, req[0])
+            ngs.sendgrouplist()
+        else:
+            ntc.sendnglist()
 
 def Listen():
-    server = SocketServer.TCPServer(('', 1178), GopherRequest)
+    server = SocketServer.TCPServer(('', 1176), GopherRequest)
     server.allow_reuse_address = 0
     server.serve_forever()
--- NNTPComm.py
+++ NNTPComm.py
@@ -12,3 +12,59 @@ class Server:
             self.outfile.write("1%s\t%s\t%s\t%s\n" % \
                                (group, group, self.myaddr[0],
                                 self.myaddr[1]))
+
+    def getoutfile(self):
+        return self.outfile
+
+    def getnntp(self):
+        return self.server
+
+class Group:
+    def __init__(self, server, group):
+        self.server = server
+        self.groupname = group
+        self.messages = []
+        self.start = None
+        self.end = None
+        
+    def getmessages(self, start=None, end=None):
+        if self.messages:
+            return self.messages
+        nntp = self.server.getnntp()
+        groupinfo = nntp.group(self.groupname)
+        self.start = groupinfo[2]
+        self.end = groupinfo[3]
+        if start == None: start = self.start
+        if end == None: end = self.end
+
+        for xover in nntp.xover(start, end)[1]:
+            self.messages.append(Message(self, xover))
+
+        return self.messages
+
+    def sendgrouplist(self):
+        self.getmessages()
+        for message in self.messages:
+            self.server.outfile.write(message.getgopherlink())
+
+class Message:
+    def __init__(self, group, xoverdata):
+        self.group = group
+        self.server = group.server
+        self.xoverdata = xoverdata
+        self.number = xoverdata[0]
+        self.subject = xoverdata[1]
+        self.poster = xoverdata[2]
+        self.date = xoverdata[3]
+        self.id = xoverdata[4]
+        self.references = xoverdata[5]
+        self.size = xoverdata[6]
+        self.lines = xoverdata[7]
+
+    def getgopherlink(self):
+        return "0%s\t%s\t%s\t%s\n" % (
+            self.subject,
+            "/" + self.group.groupname + "/" + self.number,
+            self.server.myaddr[0],
+            self.server.myaddr[1])
+    
